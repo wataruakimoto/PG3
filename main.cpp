734f4befea5549a16b05f967dab674fd97eab6c2
k023g0116@g.neec.ac.jp <k023g0116@g.neec.ac.jp>
2024-12-20 22:33:25 +0900

    06_03実装

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void PrintThread(uint32_t num) {
+
+	cout << "thread" << num << endl;
+}
+
 int main() {
+
+	// マルチスレッドではある
+	thread t1(PrintThread, 1);
+	t1.join();
+	thread t2(PrintThread, 2);
+	t2.join();
+	thread t3(PrintThread, 3);
+	t3.join();
 
 	return 0;
 }
